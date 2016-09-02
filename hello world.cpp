9e73d8dbc4525009d88caa9a8057b9600f32d237
ajanik2 <ajanik2@luc.edu>
2016-09-01 21:10:55 -0500

    Add files via upload
    hello world program

---
+++ hello world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "hello world"<<endl;
+	cin.ignore();
+	return 0;
+}
